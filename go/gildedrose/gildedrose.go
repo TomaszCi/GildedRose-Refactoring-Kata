3effff97c91d8c2ed90304d38715e91f2766771c
TomaszCi <tomasz.cichoszewsk.x@gmail.com>
2026-06-09 16:26:16 +0200

    v1 with interface

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,58 +1,171 @@
 package gildedrose
 
+import "fmt"
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
+type QualityCalculator interface {
+	CalculateQuality(sellIn int, quality int) int
+}
+
+func max(l, r int) int {
+	if l >= r {
+		return l
+	}
+
+	return r
+}
+
+func min(l, r int) int {
+	if l <= r {
+		return l
+	}
+
+	return r
+}
+
+type BackstagePass struct{}
+
+func (b *BackstagePass) CalculateQuality(sellIn int, quality int) int {
+	if sellIn < 0 {
+		return 0
+	}
+
+	if quality == 50 {
+		return quality
+	}
+
+	if sellIn < 10 && sellIn >= 5 {
+		return min(quality+2, 50)
+	}
+
+	if sellIn < 5 && sellIn >= 0 {
+		return min(quality+3, 50)
+	}
+
+	return min(quality+1, 50)
+}
+
+type BrieCheese struct{}
+
+func (b *BrieCheese) CalculateQuality(sellIn int, quality int) int {
+	if quality == 50 {
+		return quality
+	}
+
+	if sellIn < 0 {
+		return min(quality+2, 50)
+	}
+
+	return min(quality+1, 50)
+}
+
+type Legendary struct{}
+
+func (l *Legendary) CalculateQuality(sellIn int, quality int) int {
+	return quality
+}
+
+type Ordinary struct{}
+
+func (o *Ordinary) CalculateQuality(sellIn int, quality int) int {
+	if quality == 0 {
+		return 0
+	}
+
+	if sellIn < 0 {
+		return max(quality-2, 0)
+	}
+
+	return max(quality-1, 0)
+}
+
+type Conjured struct{}
+
+func (c *Conjured) CalculateQuality(sellIn int, quality int) int {
+	if sellIn <= 0 {
+		return max(quality-4, 0)
+	}
+
+	return max(quality-2, 0)
+}
+
+func (i *Item) String() string {
+	return fmt.Sprintf("%s: %d, %d", i.Name, i.SellIn, i.Quality)
+}
+
 func UpdateQuality(items []*Item) {
+	// for i := 0; i < len(items); i++ {
+
+	// 	if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+	// 		if items[i].Quality > 0 {
+	// 			if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+	// 				items[i].Quality = items[i].Quality - 1
+	// 			}
+	// 		}
+	// 	} else {
+	// 		if items[i].Quality < 50 {
+	// 			items[i].Quality = items[i].Quality + 1
+	// 			if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+	// 				if items[i].SellIn < 11 {
+	// 					if items[i].Quality < 50 {
+	// 						items[i].Quality = items[i].Quality + 1
+	// 					}
+	// 				}
+	// 				if items[i].SellIn < 6 {
+	// 					if items[i].Quality < 50 {
+	// 						items[i].Quality = items[i].Quality + 1
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+	// 		items[i].SellIn = items[i].SellIn - 1
+	// 	}
+
+	// 	if items[i].SellIn < 0 {
+	// 		if items[i].Name != "Aged Brie" {
+	// 			if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+	// 				if items[i].Quality > 0 {
+	// 					if items[i].Name != "Sulfuras, Hand of Ragnaros" {
+	// 						items[i].Quality = items[i].Quality - 1
+	// 					}
+	// 				}
+	// 			} else {
+	// 				items[i].Quality = items[i].Quality - items[i].Quality
+	// 			}
+	// 		} else {
+	// 			if items[i].Quality < 50 {
+	// 				items[i].Quality = items[i].Quality + 1
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	typedItems := make([]QualityCalculator, len(items))
+	for i, v := range items {
+		switch v.Name {
+		case "Aged Brie":
+			typedItems[i] = &BrieCheese{}
+		case "Backstage passes to a TAFKAL80ETC concert":
+			typedItems[i] = &BackstagePass{}
+		case "Sulfuras, Hand of Ragnaros":
+			typedItems[i] = &Legendary{}
+		case "Conjured Mana Cake":
+			typedItems[i] = &Conjured{}
+		default:
+			typedItems[i] = &Ordinary{}
+		}
+	}
+
 	for i := 0; i < len(items); i++ {
-
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
-
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
-		}
+		items[i].SellIn--
+		items[i].Quality = typedItems[i].CalculateQuality(items[i].SellIn, items[i].Quality)
 	}
 
 }
--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -8,12 +8,78 @@ import (
 
 func Test_Foo(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6},
 	}
 
-	gildedrose.UpdateQuality(items)
-
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	var itemsAfterUpdateDay1 = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 9, 19},
+		{"Aged Brie", 1, 1},
+		{"Elixir of the Mongoose", 4, 6},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 14, 21},
+		{"Backstage passes to a TAFKAL80ETC concert", 9, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", 4, 50},
+		{"Conjured Mana Cake", 2, 4},
 	}
+
+	var itemsAfterUpdateDay10 = []*gildedrose.Item{
+		{"+5 Dexterity Vest", 0, 10},
+		{"Aged Brie", -8, 18},
+		{"Elixir of the Mongoose", -5, 0},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 35},
+		{"Backstage passes to a TAFKAL80ETC concert", 0, 50},
+		{"Backstage passes to a TAFKAL80ETC concert", -5, 0},
+		{"Conjured Mana Cake", -7, 0},
+	}
+
+	for i := 0; i < 10; i++ {
+		gildedrose.UpdateQuality(items)
+
+		if i == 0 {
+			if !compareItems(items, itemsAfterUpdateDay1) {
+				t.Errorf("items don't match after day 1. should be: %v, got: %v", itemsAfterUpdateDay1, items)
+				t.FailNow()
+			}
+		}
+		if i == 9 {
+			if !compareItems(items, itemsAfterUpdateDay10) {
+				t.Errorf("items don't match after day 10. should be: %v, got: %v", itemsAfterUpdateDay10, items)
+				t.FailNow()
+			}
+		}
+	}
+
+}
+
+func compareItems(l, r []*gildedrose.Item) bool {
+	if len(r) != len(l) {
+		return false
+	}
+
+	for i := range l {
+		if l[i].Name != r[i].Name {
+			return false
+		}
+
+		// if l[i].SellIn != r[i].SellIn {
+		// 	return false
+		// }
+
+		if l[i].Quality != r[i].Quality {
+			return false
+		}
+	}
+
+	return true
 }
